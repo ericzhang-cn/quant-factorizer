d9fc0157db8a16f2cf24547b5a3ee0a30d350ecc
ericzhang <ericzhang.buaa@gmail.com>
2024-03-19 14:54:35 +0800

    WIP: evaluation.

--- quant_factorizer/workflow/run.py
+++ quant_factorizer/workflow/run.py
@@ -1,10 +1,13 @@
 import concurrent
+import time
 import typing
+from time import sleep
 
 import pandas as pd
 from arrow import Arrow
 from loguru import logger
 from rich.console import Console
+from rich.progress import track, Progress
 from rich.table import Table
 
 from quant_factorizer.data.base import OHLCVDataLoader, IndicatorWriter
@@ -117,7 +120,7 @@ def run_workflow(
     if verbose:
         console.print(f'[bold green]OHLCV data loaded.')
 
-        table = Table(show_header=True)
+        table = Table(show_header=True, title='OHLCV Data Summary')
         table.add_column('Symbol')
         table.add_column('Begin')
         table.add_column('End')
@@ -152,7 +155,7 @@ def run_workflow(
     if verbose:
         console.print(f'[bold green]OHLCV data filtered.')
 
-        table = Table(show_header=True)
+        table = Table(show_header=True, title='Filtered OHLCV Data Summary')
         table.add_column('Symbol')
         table.add_column('Begin')
         table.add_column('End')
@@ -172,21 +175,38 @@ def run_workflow(
         console.print(f'[bold green]Calculating indicators...')
 
     r_dfs = []
-    for symbol in df.index.get_level_values('symbol').unique():
-        r_df = _calculate(
-            df, symbol=symbol, conf=conf, concurrency=concurrency
-        )
-        if conf.factor.crosses:
-            c_df = _cross(r_df, conf=conf, concurrency=concurrency)
-            r_df = pd.concat([r_df, c_df], axis=1)
-        r_df['symbol'] = symbol
-        r_df.reset_index(inplace=True)
-        r_df.set_index(['symbol', 'time'], inplace=True)
-        r_dfs.append(r_df)
+    with Progress(console=console) as progress:
+        if verbose:
+            task_id = progress.add_task(
+                f'[cyan]Calculating Progress',
+                total=len(df.index.get_level_values('symbol').unique()),
+            )
+        for symbol in df.index.get_level_values('symbol').unique():
+            r_df = _calculate(
+                df, symbol=symbol, conf=conf, concurrency=concurrency
+            )
+            if conf.factor.crosses:
+                c_df = _cross(r_df, conf=conf, concurrency=concurrency)
+                r_df = pd.concat([r_df, c_df], axis=1)
+            r_df['symbol'] = symbol
+            r_df.reset_index(inplace=True)
+            r_df.set_index(['symbol', 'time'], inplace=True)
+            r_dfs.append(r_df)
+            if verbose:
+                progress.update(task_id, advance=1)
     calculation_df = pd.concat(r_dfs, axis=0)
 
     if verbose:
         console.print(f'[bold green]Indicators calculated.')
+
+        table = Table(show_header=True, title='Indicators Summary')
+        table.add_column('Indicator')
+
+        for col in calculation_df.columns:
+            table.add_row(
+                col,
+            )
+        console.print(table)
 
     # Evaluate indicators.
     if verbose:
@@ -194,21 +214,45 @@ def run_workflow(
 
     if conf.factor.evaluators:
         e_dfs = []
-        for symbol in calculation_df.index.get_level_values('symbol').unique():
-            e_df = _evaluate(
-                calculation_df,
-                symbol=symbol,
-                conf=conf,
-                concurrency=concurrency,
-            )
-            e_df['symbol'] = symbol
-            e_dfs.append(e_df)
+        with Progress(console=console) as progress:
+            if verbose:
+                task_id = progress.add_task(
+                    f'[cyan]Evaluating Progress',
+                    total=len(
+                        calculation_df.index.get_level_values(
+                            'symbol'
+                        ).unique()
+                    ),
+                )
+            for symbol in calculation_df.index.get_level_values(
+                'symbol'
+            ).unique():
+                e_df = _evaluate(
+                    calculation_df,
+                    symbol=symbol,
+                    conf=conf,
+                    concurrency=concurrency,
+                )
+                e_df['symbol'] = symbol
+                e_dfs.append(e_df)
+                if verbose:
+                    progress.update(task_id, advance=1)
         evaluation_df = pd.concat(e_dfs, axis=0)
     else:
         evaluation_df = pd.DataFrame()
 
     if verbose:
         console.print(f'[bold green]Indicators evaluated.')
+        table = Table(show_header=True, title='Evaluation Summary')
+
+        table.add_column('Evaluator')
+
+        for col in evaluation_df.columns:
+            table.add_row(
+                col,
+            )
+
+        console.print(table)
 
     # Write data.
     if verbose:
